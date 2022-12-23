5f7c0157cab8c314d41fa6b41e1e291dbdf60819
Andres Giraldo <giraldoma@javeriana.edu.co>
2022-12-22 23:32:03 -0500

    Initialize project

---
+++ src/main/java/com/project/multiuserdungeonbackend/MultiUserDungeonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.multiuserdungeonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiUserDungeonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiUserDungeonBackendApplication.class, args);
+	}
+
+}
